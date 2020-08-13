dc89bbfd4b9aa1d198220b1bd80edd50ca43fd63
= <harvey.alex01+github@gmail.com>
2020-08-13 12:17:26 +1000

    .

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
