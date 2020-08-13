0c471290bb6478583d62870ce33c7be6a520906c
= <harvey.alex01+github@gmail.com>
2020-08-13 11:35:12 +1000

    .

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
