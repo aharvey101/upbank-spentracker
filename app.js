2b0894a6de8135ac0431d77a087a04cd174cef42
= <harvey.alex01+github@gmail.com>
2020-08-13 10:39:36 +1000

    .

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // if in production
   if (process.env.NODE_ENV === 'production') {
@@ -29,10 +26,6 @@ app.use(function (err, req, res, next) {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
   }
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
