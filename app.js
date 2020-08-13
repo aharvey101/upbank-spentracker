25dc4a6b5c581c4258c7790d7ada1535b23d0843
= <harvey.alex01+github@gmail.com>
2020-08-13 10:35:32 +1000

    .

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
   }
